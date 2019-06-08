21ca7e90e61b376298c1ff4a08516d68daa69156
Rohan Vachheta <rohanvachheta19970@gmail.com>
2019-06-08 11:53:01 +0530

    learning commit

--- src/Root.js
+++ src/Root.js
@@ -8,7 +8,7 @@ export default ({ children, initialstate = {} }) => {
   const store = createStore(
     reducer,
     initialstate,
-    applyMiddleware(reduxPromise)
+    // applyMiddleware(reduxPromise)
   ); 
   return <Provider store={store}>{children}</Provider>;
 };
